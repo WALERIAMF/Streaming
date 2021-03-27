9a5fd65ffaace3fec759d7446bc24561e4381cc5
waleriamf <waleriamf@gmail.com>
2021-03-26 22:41:25 -0300

    swagger

--- Streaming/Models/Genero.cs
+++ Streaming/Models/Genero.cs
@@ -12,8 +12,7 @@ namespace Streaming.Models
         [Display(Name = "Descrição")]
         [Required(ErrorMessage = "A descrição é obrigatória .")]
         public string Descricao { get; set; }
-        public List<Filmes> Filmes { get; set; }
-        public List<Series> Series { get; set; }
+
 
     }
 }
